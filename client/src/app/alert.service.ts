cd4d4e4d945dbebcbd893d0aaacd9225033c2f92
Étienne Gauvin <etienne@gauvin.info>
2025-03-24 21:16:14 +0100

    Added error details display when in dev mode

--- client/src/app/alert.service.ts
+++ client/src/app/alert.service.ts
@@ -1,4 +1,4 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, isDevMode } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
@@ -15,11 +15,20 @@ export class AlertService {
   }
 
   /**
-   * Display an error message to the user
+   * Display an error message to the user.
+   * Details are displayed only in development mode
    */
-  public error(message: string, closeButtonLabel = 'OK'): void {
-    this.snackBar.open(message, closeButtonLabel, {
-      panelClass: ['error'],
-    });
+  public error(
+    message: string,
+    details: string = '',
+    closeButtonLabel = 'OK'
+  ): void {
+    this.snackBar.open(
+      message + (isDevMode() ? details : ''),
+      closeButtonLabel,
+      {
+        panelClass: ['error'],
+      }
+    );
   }
 }
