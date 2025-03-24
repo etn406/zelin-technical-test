cebf08f8056645b0ab009f6e4eac9cb4eabb3d72
Étienne Gauvin <etienne@gauvin.info>
2025-03-24 21:15:13 +0100

    Reworked the table component to use Signals

--- client/src/app/books-table/books-table.component.ts
+++ client/src/app/books-table/books-table.component.ts
@@ -2,26 +2,26 @@ import {
   AfterViewInit,
   Component,
   inject,
-  isDevMode,
+  signal,
   ViewChild,
 } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { RouterLink } from '@angular/router';
-import { catchError, map, merge, of, startWith, switchMap } from 'rxjs';
+import { merge, startWith, switchMap } from 'rxjs';
 import {
   BOOK_TABLE_DEFAULT_DISPLAYED_COLUMNS,
   BOOK_TABLE_DEFAULT_PAGE_SIZE,
   BOOK_TABLE_DEFAULT_SORT_COLUMN,
   BOOK_TABLE_DEFAULT_SORT_DIRECTION,
 } from '../../consts';
+import { Book } from '../../entities/book.schema';
 import { GetBooksParams } from '../../entities/get-books-params.interface';
-import { GetBooksResponse } from '../../entities/get-books-response.interface';
 import { BookService } from '../../services/book.service';
+import { AlertService } from '../alert.service';
 import { StarRatingComponent } from '../star-rating/star-rating.component';
 
 @Component({
@@ -39,67 +39,61 @@ import { StarRatingComponent } from '../star-rating/star-rating.component';
   ],
 })
 export class BooksTableComponent implements AfterViewInit {
-  private snackBar = inject(MatSnackBar);
-  private bookService = inject(BookService);
-  public data: GetBooksResponse = { books: [], total: 0 };
+  private readonly alertService = inject(AlertService);
+  private readonly bookService = inject(BookService);
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  readonly books = signal<Book[]>([]);
+  readonly booksTotal = signal(0);
+  readonly isLoadingResults = signal(false);
+  readonly displayedColumns = signal<(keyof Book)[]>(
+    BOOK_TABLE_DEFAULT_DISPLAYED_COLUMNS
+  );
 
-  public isLoadingResults = false;
-  public displayedColumns = BOOK_TABLE_DEFAULT_DISPLAYED_COLUMNS;
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
 
   ngAfterViewInit(): void {
-    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+    this.matSort.sortChange.subscribe(() => (this.matPaginator.pageIndex = 0));
 
-    merge(this.sort.sortChange, this.paginator.page)
+    merge(this.matSort.sortChange, this.matPaginator.page)
       .pipe(
         startWith({}),
         switchMap(() => {
-          this.isLoadingResults = true;
+          this.isLoadingResults.set(true);
           return this.bookService.getBooks(this.getBooksParams());
-        }),
-        catchError((error) => {
-          console.error(error);
-
-          if (error.name === 'HttpErrorResponse') {
-            this.showErrorSnackbar(
-              `An error occurred when requesting the books, the server may be not reacheable!`,
-              isDevMode() ? `${error.message}` : ''
-            );
-          } else if (error.name === 'ZodError') {
-            this.showErrorSnackbar(
-              `An error occurred when parsing the response!`,
-              isDevMode() ? `${error.message}` : ''
-            );
-          }
-
-          return of({ books: [], total: 0 });
-        }),
-        map((data): GetBooksResponse => {
-          this.isLoadingResults = false;
-          return data;
         })
       )
-      .subscribe((data) => {
-        this.data = data;
+      .subscribe({
+        next: (data) => {
+          this.isLoadingResults.set(false);
+          this.books.set(data.books);
+          this.booksTotal.set(data.total);
+        },
+        error: (error) => {
+          if (error.name === 'HttpErrorResponse') {
+            this.alertService.error(
+              `An error occurred when requesting the books, the server may be not reacheable!`,
+              `${error.message}`
+            );
+          } else if (error.name === 'ZodError') {
+            this.alertService.error(
+              `An error occurred when parsing the response!`,
+              `${error.message}`
+            );
+          }
+        },
       });
   }
 
   private getBooksParams(): GetBooksParams {
     return {
-      pageIndex: this.paginator?.pageIndex ?? 0,
-      pageSize: this.paginator?.pageSize ?? BOOK_TABLE_DEFAULT_PAGE_SIZE,
-      sortColumn: this.sort?.active ?? BOOK_TABLE_DEFAULT_SORT_COLUMN,
-      sortDirection: this.sort?.direction || BOOK_TABLE_DEFAULT_SORT_DIRECTION,
+      pageIndex: this.matPaginator?.pageIndex ?? 0,
+      pageSize: this.matPaginator?.pageSize ?? BOOK_TABLE_DEFAULT_PAGE_SIZE,
+      sortColumn: this.matSort?.active ?? BOOK_TABLE_DEFAULT_SORT_COLUMN,
+      sortDirection:
+        this.matSort?.direction || BOOK_TABLE_DEFAULT_SORT_DIRECTION,
+
+      deleted: false,
     };
-  }
-
-  private showErrorSnackbar(...messages: string[]): void {
-    this.snackBar.open(messages.join('\n'), 'OK');
-  }
-
-  public getRouterLinkToBookEdit(id: number): string {
-    return `/books/${id}/edit`;
   }
 }
