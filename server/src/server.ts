ed4ea981fdd3458f902cb249f01de04a24fbfda4
Étienne Gauvin <etienne@gauvin.info>
2025-03-24 16:22:18 +0100

    Fixed incorrect Express middleware order

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,24 +4,23 @@ import "reflect-metadata";
 import { CLIENT_URL, SERVER_PORT } from "./env.js";
 import { errorRequestHandler } from "./errors.js";
 import { booksRouter } from "./routers/books.router.js";
-import { statusRouter } from "./routers/status.router.js";
 
 const app = express();
 
 app.use(errorRequestHandler);
 
+// Allows the client to have a different hostname/port than the server
 app.use(
   cors({
     origin: CLIENT_URL,
   })
 );
 
-app.use("/books", booksRouter);
-app.use("/status", statusRouter);
-
+// Parse JSON body (for PUT/POST requests)
 app.use(express.json());
-app.use(express.text());
-app.use(express.urlencoded({ extended: false }));
+
+// Routes need to be registered after the middlewares
+app.use("/books", booksRouter);
 
 app.listen(SERVER_PORT, () => {
   console.log(`App is listening on port ${SERVER_PORT}`);
