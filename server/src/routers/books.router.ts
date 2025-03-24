e781b85cd7607d9d0b2efa157de3f770161ff33f
Étienne Gauvin <etienne@gauvin.info>
2025-03-24 16:23:47 +0100

    Added PUT route to update one book

--- server/src/routers/books.router.ts
+++ server/src/routers/books.router.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { z } from "zod";
+import { Book } from "../db/schema.js";
 import { GetBooksRequestParamsSchema } from "../entities/get-books-request-params.schema.js";
-import { getBooks } from "../services/books.service.js";
+import { BookUpdateSchema } from "../entities/update-book.schema.js";
+import { getBook } from "../services/get-book.js";
+import { getBooks } from "../services/get-books.js";
+import { updateBook } from "../services/update-book.js";
 
 export const booksRouter = Router();
 
+/**
+ * Get a paginated list of books
+ */
 booksRouter.get("/", async (req, res) => {
-  const params: z.infer<typeof GetBooksRequestParamsSchema> =
-    GetBooksRequestParamsSchema.parse(req.query);
+  const params = GetBooksRequestParamsSchema.parse(req.query);
 
   const [books, total] = await getBooks(
     params.pageIndex,
@@ -20,4 +26,33 @@ booksRouter.get("/", async (req, res) => {
     books,
     total,
   });
+});
+
+/**
+ * Gets one specific book
+ */
+booksRouter.get("/:id", async (req, res) => {
+  const id = z.coerce.number().parse(req.params.id);
+  const book: Book | null = await getBook(id);
+
+  if (book) {
+    res.status(200).json(book);
+  } else {
+    res.status(404);
+  }
+});
+
+/**
+ * Updates one specific book
+ */
+booksRouter.put("/:id", async (req, res) => {
+  const id = z.coerce.number().parse(req.params.id);
+  const data = BookUpdateSchema.parse(req.body);
+  const book: Book | null = await updateBook(id, data);
+
+  if (book) {
+    res.status(200).json(book);
+  } else {
+    res.status(404);
+  }
 });
