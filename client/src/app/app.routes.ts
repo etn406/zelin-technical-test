0fba99441780f8be94795f354ad554ef85e3dda1
Étienne Gauvin <etienne@gauvin.info>
2025-03-25 11:50:39 +0100

    Updated the deleted page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,21 +2,28 @@ import { Routes } from '@angular/router';
 import { AddOrEditBookComponent } from './add-or-edit-book/add-or-edit-book.component';
 import { AllBooksPageComponent } from './all-books-page/all-books-page.component';
 import { bookResolver } from './book.resolver';
+import { DeletedBooksPageComponent } from './deleted-books-page/deleted-books-page.component';
 
 export const routes: Routes = [
   { path: 'books', component: AllBooksPageComponent, title: 'All Books' },
 
   {
+    path: 'books/deleted',
+    component: DeletedBooksPageComponent,
+    title: 'Deleted Books',
+  },
+
+  {
     path: 'books/add',
     component: AddOrEditBookComponent,
-    title: 'Add a book',
+    title: 'Add a Book',
   },
 
   {
     path: 'books/:id',
     component: AddOrEditBookComponent,
     resolve: { book: bookResolver },
-    title: 'Edit a book',
+    title: 'Edit a Book',
   },
 
   { path: '', redirectTo: 'books', pathMatch: 'full' },
---
+++ client/src/app/deleted-books-page/deleted-books-page.component.ts
@@ -0,0 +1,67 @@
+import { Component, inject, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {
+  BOOK_TABLE_DEFAULT_PAGE_SIZE,
+  BOOK_TABLE_DEFAULT_SORT_COLUMN,
+  BOOK_TABLE_DEFAULT_SORT_DIRECTION,
+} from '../../consts';
+import { Book } from '../../entities/book.schema';
+import { GetBooksParams } from '../../entities/get-books-params.interface';
+import { BookService } from '../../services/book.service';
+import { AlertService } from '../alert.service';
+import { BooksTableComponent } from '../books-table/books-table.component';
+
+@Component({
+  selector: 'app-deleted-books-page',
+  imports: [BooksTableComponent, MatCardModule],
+  templateUrl: './deleted-books-page.component.html',
+  styleUrl: './deleted-books-page.component.scss',
+})
+export class DeletedBooksPageComponent {
+  private readonly alertService = inject(AlertService);
+  private readonly bookService = inject(BookService);
+
+  readonly isLoadingResults = signal(false);
+
+  readonly books = signal<Book[]>([]);
+  readonly booksTotal = signal(0);
+
+  readonly parameters = signal<GetBooksParams>({
+    pageIndex: 0,
+    pageSize: BOOK_TABLE_DEFAULT_PAGE_SIZE,
+    sortColumn: BOOK_TABLE_DEFAULT_SORT_COLUMN,
+    sortDirection: BOOK_TABLE_DEFAULT_SORT_DIRECTION,
+    deleted: true,
+  });
+
+  onParametersChange(parameters: GetBooksParams): void {
+    console.log(this.parameters());
+    this.parameters.set(parameters);
+    this.requestBooks();
+  }
+
+  private requestBooks(): void {
+    this.isLoadingResults.set(true);
+
+    this.bookService.getBooks(this.parameters()).subscribe({
+      next: (data) => {
+        this.isLoadingResults.set(false);
+        this.books.set(data.books);
+        this.booksTotal.set(data.total);
+      },
+      error: (error) => {
+        if (error.name === 'HttpErrorResponse') {
+          this.alertService.error(
+            `An error occurred when requesting the books, the server may be not reacheable!`,
+            `${error.message}`
+          );
+        } else if (error.name === 'ZodError') {
+          this.alertService.error(
+            `An error occurred when parsing the response!`,
+            `${error.message}`
+          );
+        }
+      },
+    });
+  }
+}
