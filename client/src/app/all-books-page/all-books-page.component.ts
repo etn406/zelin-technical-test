dbead739eb3822ac1f50f5a8d0132c4415b3089e
Étienne Gauvin <etienne@gauvin.info>
2025-03-25 11:50:18 +0100

    Wrapped the table with a mat-card

--- client/src/app/all-books-page/all-books-page.component.ts
+++ client/src/app/all-books-page/all-books-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {
+  BOOK_TABLE_DEFAULT_PAGE_SIZE,
+  BOOK_TABLE_DEFAULT_SORT_COLUMN,
+  BOOK_TABLE_DEFAULT_SORT_DIRECTION,
+} from '../../consts';
 import { Book } from '../../entities/book.schema';
 import { GetBooksParams } from '../../entities/get-books-params.interface';
 import { BookService } from '../../services/book.service';
@@ -7,7 +13,7 @@ import { BooksTableComponent } from '../books-table/books-table.component';
 
 @Component({
   selector: 'app-all-books-page',
-  imports: [BooksTableComponent],
+  imports: [BooksTableComponent, MatCardModule],
   templateUrl: './all-books-page.component.html',
   styleUrl: './all-books-page.component.scss',
 })
@@ -22,9 +28,9 @@ export class AllBooksPageComponent {
 
   readonly parameters = signal<GetBooksParams>({
     pageIndex: 0,
-    pageSize: 10,
-    sortColumn: 'id',
-    sortDirection: 'asc',
+    pageSize: BOOK_TABLE_DEFAULT_PAGE_SIZE,
+    sortColumn: BOOK_TABLE_DEFAULT_SORT_COLUMN,
+    sortDirection: BOOK_TABLE_DEFAULT_SORT_DIRECTION,
     deleted: false,
   });
 
